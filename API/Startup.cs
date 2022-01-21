ef7a21ff5d2b220a4fadad81fd3aaee79f02b930
Kyle Skinner <kyleskin@gmail.com>
2022-01-21 15:44:59 -0700

    starting up client

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -41,7 +42,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
